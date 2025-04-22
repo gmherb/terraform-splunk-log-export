218648a79c9f7ea193c2a6367fbf6e6225085f64
gmherb <13892557+gmherb@users.noreply.github.com>
2025-04-21 23:01:27 -0400

    remove duplicate pubsub iam binding resource now that try is use

--- permissions.tf
+++ permissions.tf
@@ -12,29 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-locals {
+resource "google_pubsub_topic_iam_binding" "input_sub_publisher" {
+  project = google_pubsub_topic.dataflow_input_pubsub_topic.project
+  topic   = google_pubsub_topic.dataflow_input_pubsub_topic.name
+  role    = "roles/pubsub.publisher"
   members = [
     try(google_logging_organization_sink.organization_log_sink[0].writer_identity,
         google_logging_project_sink.project_log_sink[0].writer_identity)
   ]
-}
-
-resource "google_pubsub_topic_iam_binding" "input_sub_publisher" {
-  count  = var.organization_id != null ? 1 : 0
-
-  project = google_pubsub_topic.dataflow_input_pubsub_topic.project
-  topic   = google_pubsub_topic.dataflow_input_pubsub_topic.name
-  role    = "roles/pubsub.publisher"
-  members = local.members
-}
-
-resource "google_pubsub_topic_iam_binding" "input_sub_publisher_org" {
-  count  = var.organization_id == null ? 1 : 0
-
-  project = google_pubsub_topic.dataflow_input_pubsub_topic.project
-  topic   = google_pubsub_topic.dataflow_input_pubsub_topic.name
-  role    = "roles/pubsub.publisher"
-  members = local.members
 }
 
 resource "google_pubsub_subscription_iam_binding" "input_sub_subscriber" {
